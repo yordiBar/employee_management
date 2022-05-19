d9b4ba8a4b24521fc5e60b91d37a3124b661675c
Bart <bart.rybak@hotmail.com>
2022-05-20 00:11:15 +0100

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace employee_management.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime StartDate { get; set; }
+        public string? PhotoFileName { get; set; }
+    }
+}
